7cbc4dba466231707550398d10e1ede5f48e7281
priykumar <66831084+priykumar@users.noreply.github.com>
2025-06-25 17:54:36 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (28.82%) - LeetSync

---
+++ Array/39-combination-sum/combination-sum.go
@@ -0,0 +1,34 @@
+import "fmt"
+
+var res [][]int
+func backtrack1(candidates, currArr []int, target, idx, sum int) {
+    if sum == target {
+        a:=make([]int, len(currArr))
+        copy(a, currArr)
+        res=append(res, a)
+        return
+        // res=append(res, currArr)
+        // return
+    }
+
+    if idx >= len(candidates) || sum > target{
+        return
+    }
+
+    // for i:=idx; i<len(candidates); i++ {
+        // not take
+        backtrack1(candidates, currArr, target, idx+1, sum)
+
+        // take
+        currArr=append(currArr, candidates[idx])
+        backtrack1(candidates, currArr, target, idx, sum+candidates[idx])
+        currArr=currArr[:len(currArr)-1]
+    // }
+}
+
+func combinationSum(candidates []int, target int) [][]int {
+    res=[][]int{}
+    currArr:=[]int{}
+    backtrack1(candidates, currArr, target, 0, 0)
+    return res
+}
