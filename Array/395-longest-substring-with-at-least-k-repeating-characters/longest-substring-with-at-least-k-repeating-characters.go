f528894a1ebc3199d146550209d9debce39ec5b6
priykumar <66831084+priykumar@users.noreply.github.com>
2025-07-09 12:30:28 +0530

    Time: 0 ms (100.00%) | Memory: 4.1 MB (72.22%) - LeetSync

--- Array/395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.go
+++ Array/395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.go
@@ -7,18 +7,19 @@ func longestSubstring(s string, k int) int {
         return len(s)
     }
 
+    // Freq of each chars in the string
     m := map[rune]int{}
     for _, e := range s {
         m[e]++
     }
 
+    // Check if any char with less than 'k' count
     invalidFound:=false
     for _, v := range m {
         if v < k {
             invalidFound = true
         }
     }
-
     if !invalidFound {
         return len(s)
     }
