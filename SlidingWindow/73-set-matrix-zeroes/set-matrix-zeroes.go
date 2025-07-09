58af0d3737fe3dff1dbe3cba15dac56da7de70ae
priykumar <66831084+priykumar@users.noreply.github.com>
2025-07-09 17:25:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (25.06%) - LeetSync

--- SlidingWindow/73-set-matrix-zeroes/set-matrix-zeroes.go
+++ SlidingWindow/73-set-matrix-zeroes/set-matrix-zeroes.go
@@ -2,6 +2,7 @@ func setZeroes(matrix [][]int)  {
     rows, cols := map[int]bool{}, map[int]bool{}
     m, n := len(matrix), len(matrix[0])
     
+    // make list of rows and cols with zeros
     for i:=0; i<m; i++ {
         for j:=0; j<n; j++ {
             if matrix[i][j] == 0 {
@@ -11,6 +12,7 @@ func setZeroes(matrix [][]int)  {
         }
     }
 
+    // populate matrix
     for i:=0; i<m; i++ {
         for j:=0; j<n; j++ {
             if rows[i] || cols[j] {
