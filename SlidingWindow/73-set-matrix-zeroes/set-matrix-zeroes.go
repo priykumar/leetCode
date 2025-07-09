4d5464013217593cd5434e185aa995c410051044
priykumar <66831084+priykumar@users.noreply.github.com>
2025-07-09 17:24:33 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (9.45%) - LeetSync

---
+++ SlidingWindow/73-set-matrix-zeroes/set-matrix-zeroes.go
@@ -0,0 +1,23 @@
+func setZeroes(matrix [][]int)  {
+    rows, cols := map[int]bool{}, map[int]bool{}
+    m, n := len(matrix), len(matrix[0])
+    
+    for i:=0; i<m; i++ {
+        for j:=0; j<n; j++ {
+            if matrix[i][j] == 0 {
+                rows[i]=true
+                cols[j]=true
+            }
+        }
+    }
+
+    for i:=0; i<m; i++ {
+        for j:=0; j<n; j++ {
+            if rows[i] || cols[j] {
+                matrix[i][j]=0
+            }
+        }
+    }
+
+    //return matrix
+}
