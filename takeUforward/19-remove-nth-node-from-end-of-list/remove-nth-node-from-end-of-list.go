128c362d1aae0603961f6bbbe1ce185639e135b3
priykumar <66831084+priykumar@users.noreply.github.com>
2025-07-10 10:52:22 +0530

    Time: 0 ms (100.00%) | Memory: 4.1 MB (92.47%) - LeetSync

--- takeUforward/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.go
+++ takeUforward/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.go
@@ -5,29 +5,46 @@
  *     Next *ListNode
  * }
  */
+// func removeNthFromEnd(head *ListNode, n int) *ListNode {
+//     sz := 0
+//     curr := head
+//     for curr != nil {
+//         sz++
+//         curr=curr.Next
+//     }
+
+//     posFromStart := sz-n+1
+//     if posFromStart == 1 {
+//         return head.Next
+//     }
+
+//     currPos:=1
+//     curr=head
+//     var prev *ListNode
+
+//     for currPos < posFromStart {
+//         currPos++
+//         prev=curr
+//         curr=curr.Next
+//     }
+
+//     prev.Next = curr.Next
+//     return head
+// }
+
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-    sz := 0
-    curr := head
-    for curr != nil {
-        sz++
-        curr=curr.Next
+    dummy := &ListNode{0, head}
+    slow, fast := dummy, dummy
+
+    for i:=0; i<=n; i++ {
+        fast=fast.Next
     }
 
-    posFromStart := sz-n+1
-    if posFromStart == 1 {
-        return head.Next
+    for fast!=nil {
+        slow=slow.Next
+        fast=fast.Next
     }
 
-    currPos:=1
-    curr=head
-    var prev *ListNode
-
-    for currPos < posFromStart {
-        currPos++
-        prev=curr
-        curr=curr.Next
-    }
-
-    prev.Next = curr.Next
-    return head
+    slow.Next = slow.Next.Next
+    return dummy.Next
 }
