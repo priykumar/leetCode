687c4296f2e144eab82e368f27ba2877b52063d1
priykumar <66831084+priykumar@users.noreply.github.com>
2025-07-10 12:55:02 +0530

    Time: 6 ms (51.04%) | Memory: 11.9 MB (55.54%) - LeetSync

---
+++ takeUforward/234-palindrome-linked-list/palindrome-linked-list.go
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func isPalindrome(head *ListNode) bool {
+    if head.Next == nil {
+        return true
+    }
+
+    var prev, l1, l2 *ListNode
+    slow, fast := head, head
+    for fast.Next != nil && fast.Next.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+    }
+
+    l1 = slow
+    l2 = slow.Next
+    // fmt.Println(l1, l2, slow)
+    isOdd:=false
+    if fast.Next == nil {
+        isOdd = true
+    }
+    slow.Next = nil
+
+    var curr, next *ListNode
+    curr, prev = head, nil
+    for curr != nil {
+        next = curr.Next
+        curr.Next=prev
+        prev=curr
+        curr=next
+    }
+
+    fmt.Println(l1, l2, isOdd)
+    // l1 = prev
+    if isOdd {
+        l1 = l1.Next
+    }
+
+    for l1 != nil && l2 != nil {
+        //fmt.Println(l1, l2)
+        if l1.Val != l2.Val {
+            return false
+        }
+        l1=l1.Next
+        l2=l2.Next
+    }
+
+    if l1==nil && l2==nil {
+        return true
+    }
+    return false
+}
