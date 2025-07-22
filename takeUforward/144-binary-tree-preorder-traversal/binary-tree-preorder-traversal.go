d856bc8a0011111187c9bad8d74a7fd880de0235
priykumar <66831084+priykumar@users.noreply.github.com>
2025-07-22 12:25:06 +0530

    Time: 0 ms (100.00%) | Memory: 4.2 MB (31.62%) - LeetSync

--- takeUforward/144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.go
+++ takeUforward/144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.go
@@ -15,8 +15,49 @@ func solve(root *TreeNode) {
     solve(root.Left)
     solve(root.Right)
 }
+
+func morris(root *TreeNode) {
+    findPredecessor := func(node *TreeNode) *TreeNode {
+        // No predecessor
+        if node.Left == nil {
+            return nil
+        }
+
+        node1 := node.Left
+        for node1.Right != nil && node1.Right != node {
+            node1 = node1.Right
+        }
+
+        return node1
+    }
+
+    curr := root
+    for curr != nil {
+        if curr.Left == nil {
+            res = append(res, curr.Val)
+            curr = curr.Right
+        } else {
+            pre := findPredecessor(curr)
+            if pre.Right == nil {
+                res = append(res, curr.Val)
+                pre.Right = curr
+                curr = curr.Left
+            } else {
+                pre.Right = nil
+                curr = curr.Right
+            }
+        }
+    }
+}
 func preorderTraversal(root *TreeNode) []int {
+    isMorris := true
     res = []int{}
-    solve(root)
+
+    if !isMorris {
+        solve(root)
+    } else {
+        fmt.Println("MORRIS TRAVERSAL")
+        morris(root)
+    }
     return res
 }
