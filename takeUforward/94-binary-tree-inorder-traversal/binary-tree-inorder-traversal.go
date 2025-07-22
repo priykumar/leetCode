d572a59c97cab55da11284aefdb576a8116e7d25
priykumar <66831084+priykumar@users.noreply.github.com>
2025-07-22 12:17:32 +0530

    Time: 0 ms (100.00%) | Memory: 4 MB (82.88%) - LeetSync

--- takeUforward/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.go
+++ takeUforward/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.go
@@ -6,6 +6,7 @@
  *     Right *TreeNode
  * }
  */
+import "fmt"
 
 var res []int
 func solve(root *TreeNode) {
@@ -16,8 +17,51 @@ func solve(root *TreeNode) {
     res = append(res, root.Val)
     solve(root.Right)
 }
+
+func morris(root *TreeNode) {
+    findPredecessor := func(node *TreeNode) *TreeNode {
+        // No predecessor
+        if node.Left == nil {
+            return nil
+        }
+
+        node1 := node.Left
+        for node1.Right != nil && node1.Right != node {
+            node1 = node1.Right
+        }
+
+        return node1
+    }
+
+    curr := root
+    for curr!=nil {
+        if curr.Left == nil {
+            res=append(res, curr.Val)
+            curr=curr.Right
+        } else {
+            pre := findPredecessor(curr)
+            // no link between curr and its predecessor, so establish a link
+            if pre.Right == nil {
+                pre.Right = curr
+                curr = curr.Left
+            } else {
+                res=append(res, curr.Val)
+                pre.Right=nil
+                curr=curr.Right
+            }
+        }
+    }
+}
+
 func inorderTraversal(root *TreeNode) []int {
+    isMorris := true
     res = []int{}
-    solve(root)
+
+    if !isMorris {
+        solve(root)
+    } else {
+        fmt.Println("MORRIS TRAVERSAL")
+        morris(root)
+    }
     return res
 }
