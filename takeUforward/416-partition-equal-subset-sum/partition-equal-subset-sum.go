c792a4f593485a6dd16fe3fdcc80a5ebd0295955
Priyanshu Kumar <66831084+priykumar@users.noreply.github.com>
2025-07-31 11:20:40 +0530

    Time: 23 ms (64.00%) | Memory: 4.7 MB (67.71%) - LeetSync

--- takeUforward/416-partition-equal-subset-sum/partition-equal-subset-sum.go
+++ takeUforward/416-partition-equal-subset-sum/partition-equal-subset-sum.go
@@ -1,77 +1,32 @@
-func tabulation(nums []int, totalSum int) []bool {
-    n := len(nums)
-
-    dp := [][]bool{}
-    temp := []bool{}
-    for i:=0; i<=totalSum; i++ {
-        temp=append(temp, false)
+func canPartition(nums []int) bool {
+    totalSum := 0
+    for i := range nums {
+        totalSum+=nums[i]
     }
-    for i:=0; i<n; i++ {
-        temp1:=make([]bool, len(temp))
-        copy(temp1, temp)
-        dp=append(dp, temp1)
+    if totalSum%2!=0 {
+        return false
     }
 
-    sort.Ints(nums)
-    for i:=0; i<n; i++ {
-        dp[i][nums[i]] = true
+    prev, curr := make([]bool, (totalSum/2)+1), make([]bool, (totalSum/2)+1)
+    prev[0]=true
+    if nums[0] < len(prev) {
+        prev[nums[0]] = true
     }
 
-    for i:=1; i<n; i++ {
-        for j:=1; j<=totalSum; j++ {
+    for i:=1; i<len(nums); i++ {
+        // fmt.Println(prev)
+        curr[0]=true
+        for j:=0; j<(totalSum/2)+1; j++ {
             // not take
-            dp[i][j] = dp[i][j] || dp[i-1][j]
-            // take
-            if j>=nums[i] {
-                dp[i][j] = dp[i][j] || dp[i-1][j-nums[i]]
-            }
-        }
-    }
-
-    // fmt.Println(dp)
-    return dp[n-1]
-}
-
-func tabulation1(nums []int, totalSum int) []bool {
-    n := len(nums)
-
-    prev, curr := []bool{}, []bool{}
-    for i:=0; i<=totalSum; i++ {
-        prev=append(prev, false)
-        curr=append(curr, false)
-    }
-
-    sort.Ints(nums)
-    prev[nums[0]] = true
-
-    for i:=1; i<n; i++ {
-        for j:=1; j<=totalSum; j++ {
-            // not take
-            curr[j] = curr[j] || prev[j]
+            curr[j]=prev[j]
             // take
             if j>=nums[i] {
                 curr[j] = curr[j] || prev[j-nums[i]]
             }
         }
-
         copy(prev, curr)
     }
-
-    // fmt.Println(dp)
-    return prev
-}
-
-func canPartition(nums []int) bool {
-    totalSum, n := 0, len(nums)
-    for i:=0; i<n; i++ {
-        totalSum += nums[i]
-    }
-
-    if totalSum%2 != 0 {
-        return false
-    }
-
-    res := tabulation1(nums, totalSum)
-    return res[totalSum/2]
-    
+    // fmt.Println()
+    // fmt.Println(prev)
+    return prev[len(prev)-1]
 }
