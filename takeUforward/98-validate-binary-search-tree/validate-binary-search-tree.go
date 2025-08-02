cabac1c19fa2740441d2fc5dbcbc3a8535deb898
Priyanshu Kumar <66831084+priykumar@users.noreply.github.com>
2025-08-03 01:16:51 +0530

    Time: 16 ms (3.16%) | Memory: 7.9 MB (10.78%) - LeetSync

---
+++ takeUforward/98-validate-binary-search-tree/validate-binary-search-tree.go
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func solve(root *TreeNode, isBST *bool) (int, int) {
+    if root == nil {
+        return math.MaxInt64, math.MinInt64 // int(1e9), int(-1e9)
+    }
+
+    lmin, lmax := solve(root.Left, isBST)
+    rmin, rmax := solve(root.Right, isBST)
+
+    if !(lmax < root.Val && root.Val < rmin) {
+        *isBST = *isBST && false
+    }
+
+    fmt.Println(root.Val, min(root.Val, min(lmin, rmin)), max(root.Val, min(lmax, rmax)))
+    return min(root.Val, min(lmin, rmin)), max(root.Val, max(lmax, rmax))
+}
+func isValidBST(root *TreeNode) bool {
+    isBST := true
+    solve(root, &isBST)
+    return isBST
+}
