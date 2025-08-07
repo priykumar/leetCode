b7a70769f8214965469e39c7e02c56e323d9937c
Priyanshu Kumar <66831084+priykumar@users.noreply.github.com>
2025-08-07 21:05:23 +0530

    Time: 1 ms (19.44%) | Memory: 8.9 MB (11.11%) - LeetSync

--- takeUforward/801-is-graph-bipartite/is-graph-bipartite.go
+++ takeUforward/801-is-graph-bipartite/is-graph-bipartite.go
@@ -18,13 +18,7 @@ func bfs(node int, color []int, graph [][]int) bool{
     return true
 }
 
-func isBipartite(graph [][]int) bool {
-    nodeCount := len(graph)
-    color := make([]int, nodeCount)
-    for i:=0; i<nodeCount; i++ {
-        color[i]=-1
-    }
-
+func isBipartite_BFS(nodeCount int, color []int, graph [][]int) bool {
     for i:=0; i<nodeCount; i++ {
         if color[i]==-1 {
             if !bfs(i, color, graph) {
@@ -34,4 +28,42 @@ func isBipartite(graph [][]int) bool {
     }
 
     return true
+}
+
+func dfs(u int, color []int, graph [][]int) bool {
+    for _, v := range graph[u] {
+        if color[v] == -1 {
+            color[v] = 1-color[u]
+            if !dfs(v, color, graph) {
+                return false
+            }
+        } else if color[v] == color[u] {
+                return false
+        }
+    }
+
+    return true
+}
+
+func isBipartite_DFS(nodeCount int, color []int, graph [][]int) bool {
+    for i:=0; i<nodeCount; i++ {
+        if color[i]==-1 {
+            color[i]=1
+            if !dfs(i, color, graph) {
+                return false
+            }
+        }
+    }
+
+    return true
+}
+
+func isBipartite(graph [][]int) bool {
+    nodeCount := len(graph)
+    color := make([]int, nodeCount)
+    for i:=0; i<nodeCount; i++ {
+        color[i]=-1
+    }
+
+    return isBipartite_DFS(nodeCount, color, graph)
 }
