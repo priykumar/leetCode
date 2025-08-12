4411284f221062bbbe2682e129b421daaf45d779
Priyanshu Kumar <66831084+priykumar@users.noreply.github.com>
2025-08-12 17:16:30 +0530

    Time: 16 ms (55.00%) | Memory: 6.6 MB (60.00%) - LeetSync

---
+++ takeUforward/1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.go
@@ -0,0 +1,40 @@
+const INT_MAX int = 1<<32
+var count int
+func recursiveApproach(l, r int, cuts []int, dp [][]int) int{
+    count++
+    if l > r {
+        return 0
+    }
+    if dp[l][r] != -1 {
+        return dp[l][r]
+    }
+
+    res := INT_MAX
+    for idx:=l; idx<=r; idx++ {
+        tempRes := cuts[r+1]-cuts[l-1] + 
+        recursiveApproach(l, idx-1, cuts, dp) + 
+        recursiveApproach(idx+1, r, cuts, dp)
+        res = min(res, tempRes)
+    }
+
+    dp[l][r] = res
+    return res
+}
+
+func minCost(n int, cuts []int) int {
+    sz := len(cuts)
+    cuts = append([]int{0}, cuts...)
+    cuts = append(cuts, n)
+    sort.Ints(cuts)
+
+    dp := make([][]int, sz+1)
+    for i:=0; i<sz+1; i++ {
+        dp[i] = make([]int, sz+1)
+        for j:=0; j<sz+1; j++ {
+            dp[i][j]=-1
+        }
+    }
+    res := recursiveApproach(1, sz, cuts, dp)
+    fmt.Println(count)
+    return res
+}
