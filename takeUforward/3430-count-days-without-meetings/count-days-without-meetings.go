52da196e68f9f9a39e1850813fe915fdcae41a3d
Priyanshu Kumar <66831084+priykumar@users.noreply.github.com>
2025-08-20 11:19:18 +0530

    Time: 246 ms (5.88%) | Memory: 20.5 MB (38.24%) - LeetSync

--- takeUforward/3430-count-days-without-meetings/count-days-without-meetings.go
+++ takeUforward/3430-count-days-without-meetings/count-days-without-meetings.go
@@ -4,8 +4,6 @@ func countDays(days int, meetings [][]int) int {
     })
 
     count:=0
-    fmt.Println(count)
-
     n := len(meetings)
     l, r := meetings[0][0], meetings[0][1]
     count+=(l-1)
@@ -19,19 +17,6 @@ func countDays(days int, meetings [][]int) int {
         }
         i++
     }
-
-    // maxEnd := meetings[0][1]
-    // for i:=1; i<len(meetings); i++ {
-    //     maxEnd = max(maxEnd, meetings[i][1])
-    //     if meetings[i-1][1]+1<meetings[i][0] {
-    //         count+=(meetings[i][0]-(meetings[i-1][1]+1))
-    //     }
-    // }
-    fmt.Println(count)
     count+=(days-r)
-    fmt.Println(count)
-    fmt.Println(meetings)
     return count
 }
-
-// [1,3] [2,4]
