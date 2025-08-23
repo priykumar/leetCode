e8fa99cabbf9ed932272d527ae8ec5979b1096bd
Priyanshu Kumar <66831084+priykumar@users.noreply.github.com>
2025-08-23 12:18:57 +0530

    Time: 106 ms (15.06%) | Memory: 5.4 MB (25.50%) - LeetSync

---
+++ dailyPractice/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
@@ -0,0 +1,19 @@
+func lengthOfLongestSubstring(s string) int {
+    char := make(map[byte]int)
+    uniqueCharCount := 0
+    start := 0
+    res:=0
+    for i:=0; i<len(s); i++ {
+        if val, exist := char[s[i]]; exist {
+            currStart := val+1
+            start=max(currStart, start)
+            uniqueCharCount=i-start+1
+        } else {
+            uniqueCharCount++
+        }
+        res = max(res, uniqueCharCount)
+        char[s[i]]=i
+        fmt.Println(i, uniqueCharCount, res)
+    }
+    return res
+}
