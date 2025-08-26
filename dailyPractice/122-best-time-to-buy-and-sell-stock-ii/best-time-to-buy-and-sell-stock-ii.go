f3cbef7c88eb1ddf4326a44f31ef4ef5a281dbe5
Priyanshu Kumar <66831084+priykumar@users.noreply.github.com>
2025-08-26 15:22:57 +0530

    Time: 2 ms (7.40%) | Memory: 5.2 MB (15.56%) - LeetSync

--- dailyPractice/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.go
+++ dailyPractice/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.go
@@ -1,69 +1,55 @@
-// func maxProfit(prices []int) int {
-//     k:=1
-//     prev, curr := make([]int, 2*k), make([]int, 2*k)
-//     for t := 0; t < 2*k; t++ {
-// 		if t%2 == 0 { // buy state
-// 			prev[t] = -prices[0]
-// 		}
-// 	}
+func maxProfit(prices []int) int {
+    prev:=[]int{-1*prices[0],0}
+    curr:=[]int{0,0}
+    for i:=1; i<len(prices); i++ {
+        for sell:=0; sell<2; sell++ {
+            if sell==0 {
+                // buy
+                curr[sell]=max(-1*prices[i]+prev[1], prev[0])
+            } else {
+                // sell
+                curr[sell]=max(prices[i]+prev[0], prev[1])
+            }
+        }
+        copy(prev, curr)
+        //fmt.Println(i, prev[0], prev[1])
+    }
+    return prev[1]
+}
 
-//     for i := 1; i < len(prices); i++ {
-//         for trans:=0; trans<2*k; trans++ {
-//             if trans%2 == 0{
-//                 // buy
-//                 if trans ==0 {
-//                     curr[trans] = max(-prices[i], prev[trans])
-//                 } else {
-//                 curr[trans] = max(-prices[i]+prev[trans-1], prev[trans])
-//                 }
-//             } else {
-//                 curr[trans] = max(prices[i]+prev[trans-1], prev[trans])
-//             }
-//         }
-//         copy(prev, curr)
+// func solve(a[]int, i int, isBuy int, dp [][]int) int {
+//     if i>=len(a) {
+//         return 0
 //     }
 
-//     // check all sell states
-//     res := 0
-//     for i:=1; i<2*k; i+=2 {
-//         res = max(res, prev[i])
+//     if (dp[i][isBuy]!=-1) {
+//         return dp[i][isBuy]
 //     }
-//     return res
+//     if isBuy==1 {
+//         dp[i][isBuy]=max(-a[i]+solve(a, i+1, 0, dp), solve(a, i+1, 1, dp))
+//     } else {
+//         dp[i][isBuy]=max(a[i]+solve(a, i+1, 1, dp), solve(a, i+1, 0, dp))
+//     }
+
+//     return dp[i][isBuy]
 // }
 
-func solve(a[]int, i int, isBuy int, dp [][]int) int {
-    if i>=len(a) {
-        return 0
-    }
+// func maxProfit(prices []int) int {
+//     n:=len(prices)
+//     if n<=1 {
+//         return 0
+//     }
 
-    if (dp[i][isBuy]!=-1) {
-        return dp[i][isBuy]
-    }
-    if isBuy==1 {
-        dp[i][isBuy]=max(-a[i]+solve(a, i+1, 0, dp), solve(a, i+1, 1, dp))
-    } else {
-        dp[i][isBuy]=max(a[i]+solve(a, i+1, 1, dp), solve(a, i+1, 0, dp))
-    }
+//     dp:=[][]int{}
+//     for i:=0; i<n; i++ {
+//         dp=append(dp, []int{-1,-1})
+//     }
+//     return solve(prices, 0, 1, dp)
+// }
 
-    return dp[i][isBuy]
-}
-
-func maxProfit(prices []int) int {
-    n:=len(prices)
-    if n<=1 {
-        return 0
-    }
-
-    dp:=[][]int{}
-    for i:=0; i<n; i++ {
-        dp=append(dp, []int{-1,-1})
-    }
-    return solve(prices, 0, 1, dp)
-}
-
-func max(a, b int) int {
-    if a>=b {
-        return a
-    }
-    return b
-}
+// func max(a, b int) int {
+//     if a>=b {
+//         return a
+//     }
+//     return b
+// }
