da077af6c465e40d114b61b62c3dd831131c7869
Priyanshu Kumar <66831084+priykumar@users.noreply.github.com>
2025-08-26 15:45:30 +0530

    Time: 5 ms (37.75%) | Memory: 8.2 MB (33.77%) - LeetSync

--- dailyPractice/188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.go
+++ dailyPractice/188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.go
@@ -38,10 +38,16 @@ func solve(a []int, i int, dp [][]int, trans, k int) int {
     if (dp[i][trans]!=-1) {
         return dp[i][trans]
     }
-    if trans%2==0 {
-        dp[i][trans]=max(-a[i]+solve(a, i+1, dp, trans+1, k), solve(a, i+1, dp, trans, k))
-    } else {
-        dp[i][trans]=max(a[i]+solve(a, i+1, dp, trans+1, k), solve(a, i+1, dp, trans, k))
+    if trans%2==0 { // buy
+        dp[i][trans]=max(
+            -a[i]+solve(a, i+1, dp, trans+1, k),  // buy
+            solve(a, i+1, dp, trans, k), // skip
+            ) 
+    } else { // sell
+        dp[i][trans]=max(
+            a[i]+solve(a, i+1, dp, trans+1, k), // sell 
+            solve(a, i+1, dp, trans, k), // skip
+            )
     }
 
     return dp[i][trans] // the maximum profit you can achieve starting from day i, given that you have already completed trans transactions
