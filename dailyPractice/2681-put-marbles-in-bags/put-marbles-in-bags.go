1a632c066fca52ecfe9924408ac1eb5aa2147e4c
Priyanshu Kumar <66831084+priykumar@users.noreply.github.com>
2025-08-26 10:11:22 +0530

    Time: 31 ms (72.73%) | Memory: 10.7 MB (45.45%) - LeetSync

--- dailyPractice/2681-put-marbles-in-bags/put-marbles-in-bags.go
+++ dailyPractice/2681-put-marbles-in-bags/put-marbles-in-bags.go
@@ -1,3 +1,4 @@
+// TC: O(logn)
 func putMarbles(weights []int, k int) int64 {
     // https://www.youtube.com/watch?v=RyJpH8cghrE
     n := len(weights)
@@ -8,7 +9,7 @@ func putMarbles(weights []int, k int) int64 {
     for i:=0; i<n-1; i++ {
         pairSum[i] = weights[i] + weights[i+1]
     }
-    sort.Ints(pairSum)
+    sort.Ints(pairSum) // If we use minheap and maxheap to get k-1 largest and k-1 smallest, then TC would be little less (O(klogn))
     var minVal, maxVal int64
     for i:=0; i<k-1; i++ {
         minVal=minVal+int64(pairSum[i])
